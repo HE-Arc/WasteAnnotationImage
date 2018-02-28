2d960bcd3db0e693b0ecb8e4abba7b6c790843b5
luc.gigon <luc.gigon@he-arc.ch>
2018-02-28 17:57:57 +0100

    model for images and little css for signup page

--- wasteannotationimage/annotation/models.py
+++ wasteannotationimage/annotation/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Contact(models.Model):
+    name = models.CharField(max_length=70)
+    image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+           return self.nom
+
 # Create your models here.
--- wasteannotationimage/wasteannotationimage/settings.py
+++ wasteannotationimage/wasteannotationimage/settings.py
@@ -135,3 +135,6 @@ STATICFILES_DIRS = (
 
 LOGIN_REDIRECT_URL = 'mainpage'
 LOGOUT_REDIRECT_URL = 'login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
