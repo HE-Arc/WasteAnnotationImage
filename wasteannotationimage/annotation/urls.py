bf99babce3babf1d6832bcc3f2827c4fe934fdef
luc.gigon <luc.gigon@he-arc.ch>
2018-03-08 13:22:00 +0100

    image for annotation ok displaying image must be fix

--- wasteannotationimage/annotation/urls.py
+++ wasteannotationimage/annotation/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('mainpage', views.mainpage, name='mainpage'),
     path('about', views.about, name='about'),
+    path('newimage', views.newImage, name='newimage'),
     ]
--- wasteannotationimage/annotation/views.py
+++ wasteannotationimage/annotation/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
+from annotation.models import Image
+from .forms import NewImage
 
 def mainpage(request):
-    return render(request, 'annotation/mainpage.html', locals())
+    images = Image.objects.all()
+    return render(request, 'annotation/mainpage.html', {'imagesObj': images})
 
 def about(request):
     return render(request, 'annotation/about.html', locals())
+
+def newImage(request):
+    isSave = False
+    form = NewImage(request.POST or None, request.FILES)
+    if form.is_valid():
+        image = Image()
+        image.name = form.cleaned_data["name"]
+        image.image = form.cleaned_data["image"]
+        image.save()
+        isSave = True
+        #return redirect('newimage')
+
+    return render(request, 'annotation/newImage.html', locals())
 
 
 
