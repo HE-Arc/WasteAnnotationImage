a770851e1be4348b567862915289670d187624f9
luc.gigon <luc.gigon@he-arc.ch>
2018-03-09 12:56:43 +0100

    petit changement temporaire

--- wasteannotationimage/wasteannotationimage/settings.py
+++ wasteannotationimage/wasteannotationimage/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^rj(u8bs-2!yz1oxwbp^7=k8j0(-x-6ts__mj*)jz%m_0mj)&r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'waibd',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'USER': '...',
+        'PASSWORD': '...',
+        'HOST': '...',
+        'PORT': '...',
     }
 }
 
